6b12ab77b18e5271c92b58c2bcca7ca949c61283
KavinZZ <55133003+pumbaX@users.noreply.github.com>
2026-05-05 17:18:31 +0200

    Update awg2.sh

--- awg2.sh
+++ awg2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-VERSION="v6.7.4"
+VERSION="v6.7.5"
 UPDATE_URL="https://raw.githubusercontent.com/pumbaX/awg-multi-script/main/awg2.sh"
 SCRIPT_PATH="/usr/local/bin/awg2"
 
